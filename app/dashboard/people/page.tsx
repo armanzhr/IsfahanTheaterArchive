eed7081dd662f9ccbcf52c676a08c5bea2bfc826
Arman <armanzahraei@gmail.com>
2024-09-01 00:41:48 +0330

    handle top of users

--- app/dashboard/people/page.tsx
+++ app/dashboard/people/page.tsx
@@ -1,7 +1,110 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <Tabs defaultValue="people" className="w-full">
+      <TabsList className="w-full">
+        <TabsTrigger className="w-full" value="roles">
+          نقش ها
+        </TabsTrigger>
+        <TabsTrigger className="w-full" value="people">
+          عوامل
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="roles">
+        <div>test</div>
+      </TabsContent>
+      <TabsContent value="people">
+        <div className="flex gap-3">
+          <Button>کاربر جدید</Button>
+          <Input
+            dir="rtl"
+            className="border focus-visible:ring-transparent"
+            placeholder="جست و جو"
+          />
+        </div>
+        <Card x-chunk="dashboard-01-chunk-5">
+          <CardHeader>
+            <CardTitle>Recent Sales</CardTitle>
+          </CardHeader>
+          <CardContent className="grid gap-8">
+            <div className="flex items-center gap-4">
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarImage src="/avatars/01.png" alt="Avatar" />
+                <AvatarFallback>OM</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-medium leading-none">
+                  Olivia Martin
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  olivia.martin@email.com
+                </p>
+              </div>
+              <div className="ml-auto font-medium">+$1,999.00</div>
+            </div>
+            <div className="flex items-center gap-4">
+              <Avatar className=" h-9 w-9 sm:flex">
+                <AvatarImage src="/avatars/02.png" alt="Avatar" />
+                <AvatarFallback>JL</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-medium leading-none">Jackson Lee</p>
+                <p className="text-sm text-muted-foreground">
+                  jackson.lee@email.com
+                </p>
+              </div>
+              <div className="ml-auto font-medium">+$39.00</div>
+            </div>
+            <div className="flex items-center gap-4">
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarImage src="/avatars/03.png" alt="Avatar" />
+                <AvatarFallback>IN</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-medium leading-none">
+                  Isabella Nguyen
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  isabella.nguyen@email.com
+                </p>
+              </div>
+              <div className="ml-auto font-medium">+$299.00</div>
+            </div>
+            <div className="flex items-center gap-4">
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarImage src="/avatars/04.png" alt="Avatar" />
+                <AvatarFallback>WK</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-medium leading-none">William Kim</p>
+                <p className="text-sm text-muted-foreground">will@email.com</p>
+              </div>
+              <div className="ml-auto font-medium">+$99.00</div>
+            </div>
+            <div className="flex items-center gap-4">
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarImage src="/avatars/05.png" alt="Avatar" />
+                <AvatarFallback>SD</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-medium leading-none">Sofia Davis</p>
+                <p className="text-sm text-muted-foreground">
+                  sofia.davis@email.com
+                </p>
+              </div>
+              <div className="ml-auto font-medium">+$39.00</div>
+            </div>
+          </CardContent>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  );
 };
 
 export default page;
