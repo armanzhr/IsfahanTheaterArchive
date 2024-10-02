8fe8ad9d79d34f67fcf444f7f9cbc1384bc4ec31
Arman <armanzahraei@gmail.com>
2024-10-02 22:59:36 +0330

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
+import { AccordionComponent } from "@/components/homepage/accordion-component";
+import BlogSample from "@/components/homepage/blog-samples";
+import HeroSection from "@/components/homepage/hero-section";
+import SideBySide from "@/components/homepage/side-by-side";
+import PageWrapper from "@/components/wrapper/page-wrapper";
 import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
-    <div className=" h-full flex flex-col items-center justify-center">
-      <p>صفحه اصلی سایت</p>
-      <p>
-        برای ورود به پنل{" "}
-        <Link href="dashboard" className="text-blue-400">
-          اینجا
-        </Link>{" "}
-        کلیک کنید
-      </p>
-    </div>
+    <PageWrapper>
+      <SideBySide />
+    </PageWrapper>
   );
 };
 
