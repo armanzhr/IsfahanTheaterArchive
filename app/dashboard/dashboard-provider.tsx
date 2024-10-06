c508824579eb0f8bff8edc65279033d752077d24
Arman <armanzahraei@gmail.com>
2024-10-06 20:27:16 +0330

    add preloader

--- app/dashboard/dashboard-provider.tsx
+++ app/dashboard/dashboard-provider.tsx
@@ -1,27 +1,53 @@
+"use client";
+import { useAuthStore } from "@/service/store/useAuthStore";
 import { cn } from "@/utils/cn";
-import React, { ReactNode } from "react";
+import { useRouter } from "next/navigation";
+import React, { ReactNode, useEffect } from "react";
+import { toast } from "sonner";
 
 const DashboardProvider = ({ children }: { children: ReactNode }) => {
+  const { preLoading, setPreLoading, getUserInfo } = useAuthStore();
+  const router = useRouter();
+
+  const getUserData = async () => {
+    try {
+      await getUserInfo();
+      setPreLoading(false);
+    } catch (error) {
+      toast.error("خطا در ورود به پنل");
+      router.push("/auth/login");
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
   return (
-    <div className=" h-full flex items-center justify-center">
-      <div className="flex items-center justify-center flex-col gap-3">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className={cn("animate-spin")}
-        >
-          <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-        </svg>
-        <p>لطفا صبر کنید...</p>
-      </div>
-    </div>
+    <>
+      {preLoading ? (
+        <div className=" h-full flex items-center justify-center">
+          <div className="flex items-center justify-center flex-col gap-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className={cn("animate-spin")}
+            >
+              <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+            </svg>
+            <p>لطفا صبر کنید...</p>
+          </div>
+        </div>
+      ) : (
+        children
+      )}
+    </>
   );
 };
 
--- service/store/useAuthStore.ts
+++ service/store/useAuthStore.ts
@@ -3,15 +3,28 @@ import { AxiosResponse } from "axios";
 import { create } from "zustand";
 import axios, { AxiosPromise } from "axios";
 import config from "@/config";
+import { getCookie } from "cookies-next";
+
+const token = getCookie("auth_token");
 
 interface AuthStore {
   authLoading: boolean;
+  preLoading: boolean;
+  setPreLoading: (status: boolean) => void;
   loginUser: (user: { username: string; password: string }) => Promise<{
     access: string;
     isSucceed: boolean;
     message: string;
     roles: string[];
   }>;
+  userInfo: {
+    id: 0;
+    userName: string;
+    firstName: string;
+    lastName: string;
+    roles: string[];
+  } | null;
+  getUserInfo: () => Promise<void>;
 }
 export const useAuthStore = create<AuthStore>((set) => ({
   authLoading: false,
@@ -24,6 +37,21 @@ export const useAuthStore = create<AuthStore>((set) => ({
       return data;
     } catch (error) {
       set({ authLoading: false });
+      throw error;
+    }
+  },
+  preLoading: true,
+  setPreLoading: (status) => set({ preLoading: status }),
+  userInfo: null,
+  getUserInfo: async () => {
+    try {
+      const { data } = await axios.get(config.baseURL + "/Auth/me", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      set({ userInfo: data });
+    } catch (error) {
       throw error;
     }
   },
