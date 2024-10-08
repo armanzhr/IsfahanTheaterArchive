5d65933146611ea4768f1eacf3a19204f5280fa0
armanzahraei <armanzahraei@gmail.com>
2024-10-08 19:14:58 +0330

    changes page

--- app/dashboard/admin/changes/changes.tsx
+++ app/dashboard/admin/changes/changes.tsx
@@ -21,16 +21,27 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { MoreHorizontal, PencilIcon, TrashIcon, UserRound } from "lucide-react";
+import {
+  CircleCheck,
+  CirclePlusIcon,
+  CircleX,
+  ClockIcon,
+  MoreHorizontal,
+  PencilIcon,
+  SquarePen,
+  TrashIcon,
+  UserRound,
+} from "lucide-react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import DeleteVenues from "@/components/pages/venues/DeleteVenues";
 import { useAuthStore } from "@/service/store/useAuthStore";
-import { Users as UsersType } from "@/utils/types";
+import { Show, Users as UsersType } from "@/utils/types";
 import { cn } from "@/utils/cn";
 import { Skeleton } from "@/components/ui/skeleton";
 import UploadUser from "@/components/pages/admin/users/UploadUser";
 import UserStatus from "@/components/pages/admin/users/userStatus";
 import { useChangesStore } from "@/service/store/useChangesStore";
+var moment = require("moment-jalaali");
 
 const Changes = () => {
   const { changesList, getChangesList } = useChangesStore();
@@ -75,84 +86,104 @@ const Changes = () => {
             <Table>
               <TableHeader>
                 <TableRow>
-                  <TableHead className="text-start">نام کاربری</TableHead>
-                  <TableHead className="text-start">نام</TableHead>
-                  <TableHead className="text-start">نام خانوادگی</TableHead>
-                  <TableHead className="text-start hidden sm:table-cell">
-                    نقش کاربری
-                  </TableHead>
+                  <TableHead className="text-start">درخواست</TableHead>
+                  <TableHead className="text-start">درخواست دهنده</TableHead>
                   <TableHead className="text-start">وضعیت</TableHead>
-
-                  <TableHead>تنظیمات</TableHead>
+                  <TableHead className="text-start">زمان درخواست</TableHead>
+                  <TableHead className="text-start">تغییرات</TableHead>
+                  <TableHead className="text-start">تعیین وضعیت</TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
                 {changesList
-                  ? changesList?.map((user) => (
-                      <TableRow
-                        key={user.id}
-                        className={cn(!user.isActive && "opacity-50")}
-                      >
-                        <TableCell className="font-medium">
-                          {user.userName}{" "}
-                          {user.id === userInfo?.id && <span>(شما)</span>}
-                        </TableCell>
-                        <TableCell>{user.firstName}</TableCell>
-                        <TableCell>{user.lastName}</TableCell>
-                        <TableCell>
-                          {user.roles.map((item) =>
-                            item === "Admin"
-                              ? "ادمین"
-                              : item === "Support"
-                              ? "پشتیبان"
-                              : "کاربر"
-                          )}
-                        </TableCell>
-                        <TableCell className=" hidden sm:table-cell">
-                          <Badge
-                            variant={user.isActive ? "secondary" : "outline"}
-                          >
-                            {user.isActive ? "فعال" : "غیرفعال"}
-                          </Badge>
-                        </TableCell>
-
-                        <TableCell className="text-end">
-                          <DropdownMenu dir="rtl">
-                            <DropdownMenuTrigger asChild>
-                              <Button
-                                aria-haspopup="true"
-                                size="icon"
-                                variant="ghost"
-                              >
-                                <MoreHorizontal className="h-4 w-4" />
-                              </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent>
-                              <DropdownMenuLabel>تنظیمات</DropdownMenuLabel>
-                              <DropdownMenuItem
-                                className="gap-2"
-                                onClick={() => handleEditUser(user)}
-                              >
-                                <PencilIcon className="w-3 h-3" />
-                                <p>ویرایش</p>
-                              </DropdownMenuItem>
-                              {userInfo?.id !== user.id && (
-                                <DropdownMenuItem
-                                  onSelect={(e) => e.preventDefault()}
-                                  className="gap-2"
-                                >
-                                  <UserRound className="w-3 h-3" />
-                                  <UserStatus
-                                    user={user}
-                                    status={user.isActive}
-                                  />
-                                </DropdownMenuItem>
+                  ? changesList?.map((change) => {
+                      const show: Show = JSON.parse(change.changes);
+                      console.log(show);
+                      return (
+                        <TableRow key={change.id}>
+                          <TableCell>
+                            <div className="font-medium flex gap-1 items-center">
+                              {change.entityId ? (
+                                <SquarePen className="w-4 h-4" />
+                              ) : (
+                                <CirclePlusIcon className="w-4 h-4" />
                               )}
-                            </DropdownMenuContent>
-                          </DropdownMenu>
-                        </TableCell>
-                      </TableRow>
-                    ))
+                              {change.entityId ? "ویرایش" : "ایجاد"}{" "}
+                              {show.title}
+                            </div>
+                          </TableCell>
+                          <TableCell>
+                            <span>
+                              {change.requesterUser.firstName}{" "}
+                              {change.requesterUser.lastName}{" "}
+                              <span dir="ltr" className="opacity-70 text-xs">
+                                (@{change.requesterUser.userName})
+                              </span>
+                            </span>
+                          </TableCell>
+
+                          <TableCell className=" hidden sm:table-cell">
+                            <Badge
+                              className={cn(
+                                change.status === 0
+                                  ? "bg-orange-400"
+                                  : change.status === 1
+                                  ? "bg-green-400"
+                                  : "bg-red-400"
+                              )}
+                              variant="default"
+                            >
+                              {change.status === 0 ? (
+                                <span className="flex gap-1 items-center">
+                                  <ClockIcon className="h-4 w-4" />
+                                  درانتظار
+                                </span>
+                              ) : change.status === 1 ? (
+                                <span className="flex gap-1 items-center">
+                                  <CircleCheck className="h-4 w-4" />
+                                  تایید شده
+                                </span>
+                              ) : (
+                                <span className="flex gap-1 items-center">
+                                  <CircleX className="h-4 w-4" />
+                                  رد شده
+                                </span>
+                              )}
+                            </Badge>
+                          </TableCell>
+                          <TableCell className="text-sm">
+                            {moment(change.createdAt).format(
+                              "jYYYY/jMM/jDD - ساعت : HH:mm"
+                            )}
+                          </TableCell>
+                          <TableCell className="text-sm">
+                            <Button variant="outline" size="sm" className="h-7">
+                              بررسی تغییرات
+                            </Button>
+                          </TableCell>
+                          <TableCell className="text-sm">
+                            <div className="flex gap-1">
+                              <Button
+                                variant="default"
+                                size="sm"
+                                className="h-7"
+                              >
+                                <CircleCheck className="w-4 h-4 ml-1" />
+                                <span className="text-xs">پذیرفتن</span>
+                              </Button>
+                              <Button
+                                variant="destructive"
+                                size="sm"
+                                className="h-7"
+                              >
+                                <CircleX className="w-4 h-4 ml-1" />
+                                رد کردن
+                              </Button>
+                            </div>
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })
                   : Array.from({ length: 5 }).map((item, index) => (
                       <TableRow key={index}>
                         <TableCell className="font-medium">
