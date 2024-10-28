aa4b9e286a3bf2cd12e91758a3127b51f435fca2
Arman <armanzahraei@gmail.com>
2024-10-28 21:10:07 +0330

    add serverside getshows

--- components/homepage/shows/shows-pagination.tsx
+++ components/homepage/shows/shows-pagination.tsx
@@ -20,12 +20,31 @@ const ShowsPaginations = ({
     <Pagination>
       <PaginationContent>
         <PaginationItem>
-          <PaginationPrevious href="#" />
+          <PaginationPrevious
+            href={`?page=${
+              Number(currentPage) > 1 ? Number(currentPage) - 1 : 1
+            }`}
+          />
         </PaginationItem>
-        {totalPages}
+
+        {/* ایجاد داینامیک PaginationItem */}
+        {Array.from({ length: Number(totalPages) }).map((_, index) => (
+          <PaginationItem key={index}>
+            <PaginationLink
+              href={`?page=${index + 1}`}
+              isActive={Number(currentPage) === index + 1}
+            >
+              {index + 1}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
 
         <PaginationItem>
-          <PaginationNext href="#" />
+          <PaginationNext
+            href={`?page=${
+              currentPage < totalPages ? currentPage + 1 : totalPages
+            }`}
+          />
         </PaginationItem>
       </PaginationContent>
     </Pagination>
